3fe6eadaf69778ffd807150f2268d46047585e27
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-06-03 01:57:23 +0530

    animated successfully

---
+++ controller.js
@@ -0,0 +1,25 @@
+const animations = {
+    run: "Running",
+    walk: "Walking",
+    jump: "Jump",
+    idle: "Idle",
+  };
+  
+  const updateAnimationMixer = () => {
+    const bot = document.getElementById("xbot-model");
+    const data = bot.getAttribute("animation-mixer");
+    console.log(data);
+    if (data.clip == animations.idle) data.clip = animations.walk;
+    else if (data.clip == animations.walk) data.clip = animations.run;
+    else data.clip = animations.walk;
+  
+    bot.setAttribute("animation-mixer", data);
+  };
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    const bot = document.getElementById("xbot-model");
+    window.setInterval(function(){
+      updateAnimationMixer();
+    }, 3000);
+  });
+  
