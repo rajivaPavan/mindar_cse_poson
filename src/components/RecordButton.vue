5db754bc2e805e6a3fe7ac35f49945c6c0a1bd81
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-06-03 20:05:47 +0530

    recording

--- src/components/RecordButton.vue
+++ src/components/RecordButton.vue
@@ -4,13 +4,86 @@ export default {
   data() {
     return {
       isRecording: false,
+      mediaRecorder: null,
+      countdownSeconds: 30,
+      countdownInterval: null,
     };
   },
   methods: {
     toggleRecording() {
       this.isRecording = !this.isRecording;
+      if (this.isRecording) {
+        this.startRecording();
+      } else {
+        this.stopRecording();
+      }
+    },
+    // This method will be called when the user clicks on the record button to ask for the screen recording permission
+    async recordScreen() {
+      return await navigator.mediaDevices.getDisplayMedia({
+        audio: true,
+        video: { mediaSource: "screen"}
+      });
+    },
+    createRecorder (stream, mimeType) {
+      // the stream data is stored in this array
+      let recordedChunks = [];
+
+      const mediaRecorder = new MediaRecorder(stream);
+
+      mediaRecorder.ondataavailable = function (e) {
+        if (e.data.size > 0) {
+          recordedChunks.push(e.data);
+        }
+      };
+      mediaRecorder.onstop = function () {
+        this.saveFile(recordedChunks);
+        recordedChunks = [];
+      };
+      mediaRecorder.start(200); // For every 200ms the stream data will be stored in a separate chunk.
+      return mediaRecorder;
+    },
+    saveFile(recordedChunks){
+
+      const blob = new Blob(recordedChunks, {
+        type: 'video/webm'
+      });
+      let filename = window.prompt('Enter file name'),
+          downloadLink = document.createElement('a');
+      downloadLink.href = URL.createObjectURL(blob);
+      downloadLink.download = `＄{filename}.webm`;
+
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      URL.revokeObjectURL(blob); // clear from memory
+      document.body.removeChild(downloadLink);
+    },
+
+    // This method will be called when the user clicks on the record button to start the recording
+    // and starts the countdown animation
+    startRecording() {
+      this.recordScreen().then((stream) => {
+        let mimeType = 'video/webm';
+        this.mediaRecorder = this.createRecorder(stream, mimeType);
+
+        // Start the countdown animation
+        this.countdownInterval = setInterval(() => {
+          this.countdownSeconds--;
+          if (this.countdownSeconds <= 0) {
+            this.stopRecording();
+          }
+        }, 1000);
+      });
+
+    },
+    // This method will be called when the user clicks on the record button to stop the recording
+    // and clears the countdown animation
+    stopRecording() {
+      clearInterval(this.countdownInterval);
+      this.mediaRecorder.stop();
     },
   },
+
 }
 </script>
 
@@ -18,7 +91,7 @@ export default {
   <div class="record-button-container">
     <div class="record-button" @click="toggleRecording">
       <div class="inner-circle" :class="{ recording: isRecording }">
-        <div class="outer-circle"></div>
+        <div class="outer-circle" :class="{ countdown: isRecording }"></div>
       </div>
     </div>
   </div>
@@ -52,9 +125,6 @@ export default {
   height: 60px;
   border-radius: 50%;
   background-color: white;
-  display: flex;
-  align-items: center;
-  justify-content: center;
   transition: background-color 0.3s ease;
 }
 
@@ -67,6 +137,24 @@ export default {
   height: 70px;
   border-radius: 50%;
   border: 5px solid white;
+/*  center this */
+  position: absolute;
+  top: -5px;
+  left: -5px;
+  transition: border-color 0.3s ease;
+}
+
+.outer-circle.countdown {
+  animation: countdown-animation 30s linear forwards;
+}
+
+@keyframes countdown-animation {
+  0% {
+    stroke-dashoffset: 0;
+  }
+  100% {
+    stroke-dashoffset: 157; /* 2 * pi * radius = 2 * 3.14 * 25 = 157 */
+  }
 }
 </style>
 
