51edc455c6d90896655a85da4df1815ebfdfa24a
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-06-03 14:31:29 +0530

    started new clean vue project for cse poson ar

---
+++ src/components/Deer.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "Deer.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/King.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "King.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
