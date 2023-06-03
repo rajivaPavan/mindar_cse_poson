ed8e35126035157785f24110144b30ab1047bae2
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-06-03 20:05:34 +0530

    updated AR scene

--- src/components/ARScene.vue
+++ src/components/ARScene.vue
@@ -1,10 +1,23 @@
 <template>
-
+  <a-scene
+      mindar-image="imageTargetSrc: ./targets/generated.mind; filterMinCF:0.0000001; filterBeta: 0.0001;"
+      color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false"
+      device-orientation-permission-ui="enabled: false">
+    <a-assets>
+      <a-asset-item id="avatarModel" src="./3d-models/deer.glb">
+      </a-asset-item>
+    </a-assets>
+    <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
+    <a-entity mindar-image-target="targetIndex: 0">
+      <a-gltf-model src="#avatarModel" id="xbot-model" rotation="0 0 0"
+                    position="0 0 0" scale="0.1 0.1 0.1" animation-mixer></a-gltf-model>
+    </a-entity>
+  </a-scene>
 </template>
 
 <script>
 export default {
-  name: "ARScene.vue"
+  name: "ARScene"
 }
 </script>
 
