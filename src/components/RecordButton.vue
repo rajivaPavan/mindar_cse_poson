6a807cdaea204009eab6c1d2e30c504a450283c3
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-06-03 18:11:01 +0530

    working on recording button

---
+++ src/components/RecordButton.vue
@@ -0,0 +1,72 @@
+<script>
+export default {
+  name: "RecordButton",
+  data() {
+    return {
+      isRecording: false,
+    };
+  },
+  methods: {
+    toggleRecording() {
+      this.isRecording = !this.isRecording;
+    },
+  },
+}
+</script>
+
+<template>
+  <div class="record-button-container">
+    <div class="record-button" @click="toggleRecording">
+      <div class="inner-circle" :class="{ recording: isRecording }">
+        <div class="outer-circle"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.record-button-container {
+  position: fixed;
+  bottom: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  z-index: 9999;
+}
+
+.record-button {
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+  background-color: red;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+}
+
+.inner-circle {
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: background-color 0.3s ease;
+}
+
+.inner-circle.recording {
+  background-color: red;
+}
+
+.outer-circle {
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+  border: 5px solid white;
+}
+</style>
+
--- src/pages/MobilePage.vue
+++ src/pages/MobilePage.vue
@@ -1,3 +1,26 @@
+<script>
+import RecordButton from '../components/RecordButton.vue';
+
+export default {
+  name: "MobilePage.vue",
+  components: {
+    RecordButton
+  }
+}
+</script>
 <template>
-  Mobile Page
+  <div class="mobile-page">
+    <!-- Content of your mobile page -->
+    <RecordButton />
+  </div>
 </template>
+
+<style scoped>
+.mobile-page {
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+  margin: 0;
+  /* Add any other styles for your mobile page */
+}
+</style>
